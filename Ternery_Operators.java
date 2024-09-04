e180f57f0b8d23ce11851e3b257a720a7dd9815b
imrohitsampannavar45 <imrohitsampannavar@gmail.com>
2024-09-04 12:33:57 +0530

    Changes Made ot the Code

---
+++ Ternery_Operators.java
@@ -0,0 +1,13 @@
+public class Ternary_Operators{
+
+
+
+
+    public Static Void main(String[] args){
+
+//A ternary Operator Evaluates the test Conidtion and Executes a block of Code Based on the Result of the Condition
+
+// Condition   ? expression 1 : expression 2;
+
+    }
+}
