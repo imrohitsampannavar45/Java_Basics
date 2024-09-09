59547be573a6f9434b00935e02121cfa5b340983
imrohitsampannavar45 <imrohitsampannavar@gmail.com>
2024-09-09 17:17:42 +0530

    no changes

---
+++ Loops/Loops.java
@@ -0,0 +1,26 @@
+public class Loops{
+
+
+
+    public static  void main(String[] args){
+
+        // for loops
+        // while loops
+        // do while loops
+        // break and contiune Statements
+        // Nested loops
+        // labelled break and continue Statements
+
+
+        //elements of java loops
+        
+        // * Initialization Expressions
+        // * Test Expression (condition)
+        // * Update Expression
+        // * Body of the loop
+
+
+
+
+    }
+}
