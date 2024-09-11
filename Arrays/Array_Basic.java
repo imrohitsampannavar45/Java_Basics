2b0f7a99170e24d66f4e81c6e2a0452577e12bd4
imrohitsampannavar45 <imrohitsampannavar@gmail.com>
2024-09-11 11:27:17 +0530

    New Topic

---
+++ Arrays/Array_Basic.java
@@ -0,0 +1,24 @@
+public class Array_Basic{
+
+    public static void main(String[] args){
+
+// How do the Arrays work?
+// Creating and Declaring Arrays
+// For-Each Loop
+// Multi Dimensional Arrays
+
+// int age[]; //declaration
+
+// age = new int[10]; //allocation
+
+int age[] = new int[5]; //both declaration and allocation
+age [0] =  5;
+age [1] = 2;
+age[2] = 10;
+age[4]= 99;
+System.out.println(age[3]);
+
+
+
+    }
+}
